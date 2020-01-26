da24d88d346a1e90d2784fb782d5415d16197bbd
Justin Yau <justiceman8998@gmail.com>
2020-01-26 17:08:46 -0500

    Made some small changes

--- src/App.js
+++ src/App.js
@@ -11,7 +11,7 @@ import {Work} from "./components/work/Work";
 
 function App() {
   return (
-    <BrowserRouter>
+    <BrowserRouter basename={process.env.PUBLIC_URL}>
       <div className="row p-0 m-0">
         <div className="app-container">
           <Header />
